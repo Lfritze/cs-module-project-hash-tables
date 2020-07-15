acc6e9f13dab9038c383f34eb8f9e16d5838b5c0
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-07-15 17:49:00 -0400

    histo.py complete

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,35 @@
 # Your code here
+ignore = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
+def histogram():
+  counts = {}
+
+  with open("robin.txt") as document:
+    wording = document.read()
+    word_splitter = wording.split()
+
+  for word in word_splitter:
+    histo_gramo = ""
+    for char in word:
+      if char not in ignore:
+        histo_gramo += char
+    word = histo_gramo.lower()
+
+    if word in counts:
+      counts[word] += 1
+    elif word == "" or word == " ":
+      break
+    else:
+      counts[word] = 1
+
+    items = list(counts.items())
+    items.sort(key = lambda e: (-e[1], e[0]))
+    counts = (dict(items))
+    for (string, value) in counts.items():
+        max_len = len(max(string, key=len))
+        print(f'{string} {" " * max_len} {"#" * value}')
+
+
+print(histogram())
+
 
